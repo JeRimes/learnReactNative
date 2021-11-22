ee23a8f1d0e759b5df801bf5922d55fb3db6f1d4
Jeremy <jeremy@yasserkidiwe.onmicrosoft.com>
2021-11-22 08:49:28 +0100

    Exercice 1

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Hello world !</Text>
     </View>
   );
 }
@@ -18,4 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+  }
 });
