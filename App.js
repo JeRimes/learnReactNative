2ba7069701727cc43ad6519b1a82516e3e6cca05
Jeremy <jeremy@yasserkidiwe.onmicrosoft.com>
2021-11-22 16:09:53 +0100

    component to add element in list

--- App.js
+++ App.js
@@ -1,43 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, ScrollView } from 'react-native';
-import { SquarePink, SquareRed } from './Components/Square'
+import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import UselessTextInput from './Components/Square'
 
 export default function App() {
   return (
     <View style={styles.main}>
-      <SquarePink />
-      <View style={styles.squareRedFlex}>
-        <SquareRed />
-      </View>
-      <SquarePink />
-      <View style={styles.squareRedFlex}>
-        <SquareRed />
-        <SquareRed />
-      </View>
-      <SquarePink />
-      <View style={styles.squareRedFlex}>
-        <SquareRed />
-        <SquareRed />
-        <SquareRed />
-      </View>
-      <SquarePink />
+      <UselessTextInput></UselessTextInput>
     </View>
   );
 
 }
 
 const styles = StyleSheet.create({
-
-  squareRedFlex: {
-    flex: 0,
-    flexDirection: 'row',
-    justifyContent: 'flex-start'
-  },
   main: {
     flex: 1,
     flexDirection: 'column',
-    alignContent: 'space-around'
+    alignContent: 'space-around',
+    marginTop: 30
   },
 
 });
--- Components/Square.js
+++ Components/Square.js
@@ -1,29 +1,64 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
-export const SquarePink = () => {
-    return (
-        <View style={styles.squarePink}></View>
-    );
-}
+import React, { useState } from "react";
+import { View, StyleSheet, TextInput, Button, Text, FlatList, TouchableOpacity } from "react-native";
 
-export const SquareRed = () => {
+
+const UselessTextInput = () => {
+    const [text, setText] = useState("");
+
+    const [pays, setPays, onPress] = useState([]);
+
+    const addTip = (value) => {
+        var item = { id: (pays.length + 1).toString(), title: value, stroke: false };
+        setPays([...pays, item]);
+    }
+    // const Item = ({ item, onPress, backgroundColor, textColor }) => (
+    //     <TouchableOpacity onPress={onPress} style={[styles.item, backgroundColor]}>
+    //         <Text style={styles.item}>{item.title}</Text>
+    //     </TouchableOpacity>
+    // );
+    // const backgroundColor = item.id === selectedId ? "#6e3b6e" : "#f9c2ff";
+    // const color = item.id === selectedId ? 'white' : 'black';
     return (
-        <View style={styles.squareRed}></View>
+        <View>
+            <TextInput
+                style={styles.input}
+                onChangeText={(val) => setText(val)}
+                value={text}
+            />
+            <Text>{text}</Text>
+            <Button
+                onPress={() =>
+                    addTip(text)
+                }
+                title="Ajouter"
+                color="#841584"
+            />
+            <FlatList
+                data={pays}
+                renderItem={({ item }) =>
+                    <Text style={styles.item}>{item.title}</Text>
+                }
+                keyExtractor={(item) => item.id}
+            />
+        </View>
     );
-}
+};
 
 const styles = StyleSheet.create({
-    squarePink: {
-        backgroundColor: 'pink',
-        width: 200,
-        height: 100,
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+    item: {
         marginTop: 10,
-        marginBottom: 10
-    },
-    squareRed: {
-        backgroundColor: 'red',
-        width: 100,
-        height: 100,
-        marginRight: 10,
-    },
+        fontSize: 20,
+        textAlign: 'center',
+        backgroundColor: "red",
+        color: 'white',
+        padding: 10
+    }
 });
+
+export default UselessTextInput;
